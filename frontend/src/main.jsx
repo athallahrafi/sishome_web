d6c7406c4c17a988fe2782c7cccb878b8b05ecf5
Rafi Athallah <rafiathallah75@gmail.com>
2026-06-28 16:28:04 +0700

    fixes:console

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const GOOGLE_CLIENT_ID = '308867522259-d3vnpt26tlv3qpbu8m52e31jmifo11vp.apps.googleusercontent.com';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
 )
